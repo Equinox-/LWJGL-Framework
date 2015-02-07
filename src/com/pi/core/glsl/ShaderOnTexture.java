9f368c536f1dd87fa2f10f92a36f1bf94b9c9e80
Westin Miller <equinoxscripts@gmail.com>
2015-02-07 09:30:58 -0800

    Use a shader to render onto a texture. Basically a FBO + Texture Reference + Set of render calls.

---
+++ src/com/pi/core/glsl/ShaderOnTexture.java
@@ -0,0 +1,45 @@
+package com.pi.core.glsl;
+
+import org.lwjgl.opengl.GL11;
+
+import com.pi.core.framebuffer.FrameBuffer;
+import com.pi.core.model.BasicShapes;
+import com.pi.core.texture.Texture;
+import com.pi.core.util.GPUObject;
+
+public class ShaderOnTexture implements GPUObject {
+	private FrameBuffer fbo;
+	private Texture result;
+
+	public ShaderOnTexture(Texture result) {
+		this.result = result;
+		this.fbo = new FrameBuffer();
+		this.fbo.attachColor(result);
+	}
+
+	@Override
+	public void gpuAlloc() {
+		this.result.gpuAlloc();
+		this.fbo.gpuAlloc();
+	}
+
+	@Override
+	public void gpuFree() {
+		this.fbo.gpuFree();
+		this.result.gpuFree();
+	}
+
+	public Texture getResult() {
+		return result;
+	}
+
+	public void render(ShaderProgram prog) {
+		GL11.glViewport(0, 0, result.getWidth(), result.getHeight());
+		fbo.bind();
+		GL11.glClear(GL11.GL_COLOR_BUFFER_BIT | GL11.GL_DEPTH_BUFFER_BIT);
+		prog.bind();
+		BasicShapes.shapes().getNDCScreenQuad().render();
+		ShaderProgram.unbind();
+		FrameBuffer.unbind();
+	}
+}
