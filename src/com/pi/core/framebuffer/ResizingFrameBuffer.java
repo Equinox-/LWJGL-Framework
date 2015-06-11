cf3c009d6d485ea1b30cff9ba877dd7d268558f3
Westin Miller <equinoxscripts@gmail.com>
2015-06-11 08:08:49 -0700

    ResizingFrameBuffer: Like WindowFrameBuffer, but custom size.

---
+++ src/com/pi/core/framebuffer/ResizingFrameBuffer.java
@@ -0,0 +1,84 @@
+package com.pi.core.framebuffer;
+
+import org.lwjgl.opengl.GL11;
+import org.lwjgl.opengl.GL30;
+
+import com.pi.core.texture.Texture;
+import com.pi.core.util.Bindable;
+import com.pi.core.util.GPUObject;
+
+public class ResizingFrameBuffer extends GPUObject<ResizingFrameBuffer>
+		implements Bindable {
+	private FrameBuffer target;
+	private Texture color;
+	private Texture depth;
+
+	public ResizingFrameBuffer() {
+	}
+
+	private int width, height;
+
+	public void size(int w, int h) {
+		this.width = w;
+		this.height = h;
+	}
+
+	protected void verify() {
+		if (target == null || color.getWidth() != width
+				|| color.getHeight() != height) {
+			if (target != null) {
+				target.gpuFree();
+				color.gpuFree();
+				depth.gpuFree();
+			}
+			color = new Texture(width, height, GL11.GL_RGB).gpuAlloc();
+			depth = new Texture(width, height, GL30.GL_DEPTH_COMPONENT32F)
+					.gpuAlloc();
+			target = new FrameBuffer();
+			target.attachColor(color);
+			target.attachDepth(depth);
+			target.gpuAlloc();
+		}
+	}
+
+	@Override
+	protected void gpuAllocInternal() {
+		// Let verify do its work
+	}
+
+	@Override
+	protected void gpuFreeInternal() {
+		if (target != null)
+			target.gpuFree();
+		if (color != null)
+			color.gpuFree();
+		if (depth != null)
+			depth.gpuFree();
+	}
+
+	@Override
+	protected ResizingFrameBuffer me() {
+		return this;
+	}
+
+	@Override
+	public void bind() {
+		verify();
+		target.bind();
+	}
+
+	public Texture color() {
+		verify();
+		return color;
+	}
+
+	public Texture depth() {
+		verify();
+		return depth;
+	}
+
+	public FrameBuffer fbo() {
+		verify();
+		return target;
+	}
+}
--- src/com/pi/core/framebuffer/WindowFrameBuffer.java
+++ src/com/pi/core/framebuffer/WindowFrameBuffer.java
@@ -1,18 +1,8 @@
 package com.pi.core.framebuffer;
 
-import org.lwjgl.opengl.GL11;
-import org.lwjgl.opengl.GL30;
-
-import com.pi.core.texture.Texture;
-import com.pi.core.util.Bindable;
-import com.pi.core.util.GPUObject;
 import com.pi.core.wind.GLWindow;
 
-public class WindowFrameBuffer extends GPUObject<WindowFrameBuffer> implements
-		Bindable {
-	private FrameBuffer target;
-	private Texture color;
-	private Texture depth;
+public class WindowFrameBuffer extends ResizingFrameBuffer {
 	private final GLWindow gl;
 
 	public WindowFrameBuffer(GLWindow gl) {
@@ -39,64 +29,16 @@ public class WindowFrameBuffer extends GPUObject<WindowFrameBuffer> implements
 		return shiftSize;
 	}
 
-	private void verify() {
-		int desW = gl.getEvents().getWidth() << shiftSize;
-		int desH = gl.getEvents().getHeight() << shiftSize;
-		if (target == null || color.getWidth() != desW
-				|| color.getHeight() != desH) {
-			if (target != null) {
-				target.gpuFree();
-				color.gpuFree();
-				depth.gpuFree();
-			}
-			color = new Texture(desW, desH, GL11.GL_RGB).gpuAlloc();
-			depth = new Texture(desW, desH, GL30.GL_DEPTH_COMPONENT32F)
-					.gpuAlloc();
-			target = new FrameBuffer();
-			target.attachColor(color);
-			target.attachDepth(depth);
-			target.gpuAlloc();
-		}
+	@Override
+	public void size(int w, int h) {
+		throw new UnsupportedOperationException(
+				"Can only resize non-window framebuffers");
 	}
 
 	@Override
-	protected void gpuAllocInternal() {
-		// Let verify do its work
-	}
-
-	@Override
-	protected void gpuFreeInternal() {
-		if (target != null)
-			target.gpuFree();
-		if (color != null)
-			color.gpuFree();
-		if (depth != null)
-			depth.gpuFree();
-	}
-
-	@Override
-	protected WindowFrameBuffer me() {
-		return this;
-	}
-
-	@Override
-	public void bind() {
-		verify();
-		target.bind();
-	}
-
-	public Texture color() {
-		verify();
-		return color;
-	}
-
-	public Texture depth() {
-		verify();
-		return depth;
-	}
-
-	public FrameBuffer fbo() {
-		verify();
-		return target;
+	protected void verify() {
+		super.size(gl.getEvents().getWidth() << shiftSize, gl.getEvents()
+				.getHeight() << shiftSize);
+		super.verify();
 	}
 }
